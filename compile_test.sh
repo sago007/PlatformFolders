0e2c2ac6b06f92cf81b10ff62f2a02a8b93202ee
sago007 <poul@poulsander.com>
2016-10-08 19:05:33 +0200

    Updated the text script to work with MXE

--- compile_test.sh
+++ compile_test.sh
@@ -4,9 +4,9 @@ set -e
 make clean
 make TESTCPP11=1
 make clean
-make CROSS=i686-pc-mingw32- TESTCPP11=1
+make CROSS=i686-w64-mingw32.static- TESTCPP11=1
 make clean
-make CROSS=i686-pc-mingw32-
+make CROSS=i686-w64-mingw32.static-
 mv platform_folders.out platform_folders.exe
 make clean
 make
