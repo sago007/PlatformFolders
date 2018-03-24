d2b67ceca589c5dcf919b528b6ae80eb96525d44
sago007 <poul@poulsander.com>
2018-03-24 10:52:39 +0100

    Fixed indentation

--- sago/platform_folders.cpp
+++ sago/platform_folders.cpp
@@ -1,29 +1,29 @@
 /*
-  Its is under the MIT license, to encourage reuse by cut-and-paste.
+Its is under the MIT license, to encourage reuse by cut-and-paste.
 
-  The original files are hosted here: https://github.com/sago007/PlatformFolders
+The original files are hosted here: https://github.com/sago007/PlatformFolders
 
-  Copyright (c) 2015-2016 Poul Sander
+Copyright (c) 2015-2016 Poul Sander
 
-  Permission is hereby granted, free of charge, to any person
-  obtaining a copy of this software and associated documentation files
-  (the "Software"), to deal in the Software without restriction,
-  including without limitation the rights to use, copy, modify, merge,
-  publish, distribute, sublicense, and/or sell copies of the Software,
-  and to permit persons to whom the Software is furnished to do so,
-  subject to the following conditions:
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 */
 
 #include "platform_folders.h"
@@ -85,12 +85,12 @@ static std::string GetAppDataLocal() {
 static std::string GetMacFolder(OSType folderType, const char* errorMsg) {
 	std::string ret;
 	FSRef ref;
-    char path[PATH_MAX];
-    OSStatus err = FSFindFolder( kUserDomain, folderType, kCreateFolder, &ref );
+	char path[PATH_MAX];
+	OSStatus err = FSFindFolder( kUserDomain, folderType, kCreateFolder, &ref );
 	if (err != noErr) {
 		throw std::runtime_error(errorMsg);
 	}
-    FSRefMakePath( &ref, (UInt8*)&path, PATH_MAX );
+	FSRefMakePath( &ref, (UInt8*)&path, PATH_MAX );
 	ret = path;
 	return ret;
 }
