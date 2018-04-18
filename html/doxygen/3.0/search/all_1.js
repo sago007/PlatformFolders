4b4f1e3e4a33faac2e16861a71ef9240dc9e9da4
sago007 <poul@poulsander.com>
2018-04-18 21:00:21 +0200

    Added Doxygen for version 3.0

---
+++ html/doxygen/3.0/search/all_1.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['getcachedir',['getCacheDir',['../namespacesago.html#a426458c0e30383f23ecab17bdf55a407',1,'sago']]],
+  ['getconfighome',['getConfigHome',['../namespacesago.html#aa6c6da4e6fe460076ac069e176622f59',1,'sago']]],
+  ['getdatahome',['getDataHome',['../namespacesago.html#a85c382a039426b76aa408961ac2149ee',1,'sago']]],
+  ['getdesktopfolder',['getDesktopFolder',['../classsago_1_1PlatformFolders.html#a6aca8cb29f7a2c48ca4f99438e3ed175',1,'sago::PlatformFolders']]],
+  ['getdocumentsfolder',['getDocumentsFolder',['../classsago_1_1PlatformFolders.html#aa4c7f57a44e5cd6849c06ba6cfa5288e',1,'sago::PlatformFolders']]],
+  ['getdownloadfolder1',['getDownloadFolder1',['../classsago_1_1PlatformFolders.html#a6142e940eb75c6eb0c13c5bc40401041',1,'sago::PlatformFolders']]],
+  ['getmusicfolder',['getMusicFolder',['../classsago_1_1PlatformFolders.html#a6bb0d7cc44a9f253610aba7af873b68a',1,'sago::PlatformFolders']]],
+  ['getpicturesfolder',['getPicturesFolder',['../classsago_1_1PlatformFolders.html#a573bcf35075a58e91012d902add73d91',1,'sago::PlatformFolders']]],
+  ['getsavegamesfolder1',['getSaveGamesFolder1',['../classsago_1_1PlatformFolders.html#ad1d62d1c68c143308f1703d1033265f6',1,'sago::PlatformFolders']]],
+  ['getvideofolder',['getVideoFolder',['../classsago_1_1PlatformFolders.html#a4aee4289ce9dea7a41a00f676dd2d800',1,'sago::PlatformFolders']]]
+];
