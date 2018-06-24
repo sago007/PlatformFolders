7e54d64b263728d5108153ce82241394f0778066
sago007 <poul@poulsander.com>
2018-06-24 17:21:10 +0200

    Added docs for 4.0.0

---
+++ html/doxygen/4.0.0/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sago',['sago',['../namespacesago.html',1,'']]]
+];
---
+++ html/doxygen/4.0.0/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "ags",
+  1: "s",
+  2: "ag"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "namespaces",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Namespaces",
+  2: "Functions"
+};
+
